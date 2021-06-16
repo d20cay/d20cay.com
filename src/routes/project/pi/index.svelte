5ad58320ec7ef2c3d8c51b0bab5ff78044b0254a
d20cayt <~J4m"D}h^Mc3>
2021-06-16 13:54:00 +0200

    Adds pi calculating logic.

--- src/routes/project/pi/index.svelte
+++ src/routes/project/pi/index.svelte
@@ -11,50 +11,73 @@
 
 	let canvas;
 	let context;
+
 	let status = Status.IDLE;
 	let calculationStarted = false;
+
 	let delay = 10;
 	let delayValid = true;
+
 	let pi = 0;
+	let points = [];
+	let piPoints = [];
+
+	let interval;
 
 	onMount(() => {
 		context = canvas.getContext('2d');
 	});
 
 	function calc() {
-	    if (delay !== Math.floor(delay)) {
-	        return;
-        }
+		if (delay !== Math.floor(delay)) {
+			return;
+		}
 		status = Status.RUNNING;
 		calculationStarted = true;
+		updatePi();
+		interval = setInterval(updatePi, delay);
 	}
 
 	function pauseCalc() {
 		status = Status.IDLE;
+		clearInterval(interval);
 	}
 
 	function stopCalc() {
 		status = Status.IDLE;
 		calculationStarted = false;
+		clearInterval(interval);
+		pi = 0;
+		points = [];
 	}
 
-	// UTIL
+	function updatePi() {
+		const x = (Math.random() - 0.5) * 2;
+		const y = (Math.random() - 0.5) * 2;
+		points.push({x, y});
+		if (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= 1) {
+			piPoints.push({x, y});
+		}
+		pi = 4 * piPoints.length / points.length;
+	}
+
+	// over-the-top UX UTIL
 
 	let runButtonTooltip = 'Run calculation';
 
-    function updateRunButtonTooltip(delay) {
-        if (delay === Math.floor(delay)) {
-            if (calculationStarted) {
-                runButtonTooltip = 'Continue calculation';
-            } else {
-                runButtonTooltip = 'Run calculation';
-            }
-        } else {
-            runButtonTooltip = 'Cannot run calculation with floating point delay'
-        }
-    }
+	function updateRunButtonTooltip(delay) {
+		if (delay === Math.floor(delay)) {
+			if (calculationStarted) {
+				runButtonTooltip = 'Continue calculation';
+			} else {
+				runButtonTooltip = 'Run calculation';
+			}
+		} else {
+			runButtonTooltip = 'Cannot run calculation with floating point delay'
+		}
+	}
 
-    $: updateRunButtonTooltip(delay)
+	$: updateRunButtonTooltip(delay)
 </script>
 
 <style>
@@ -92,16 +115,16 @@
 				       step="1"
 				       bind:value={delay}
 				       class="uk-input uk-border-rounded"
-					   class:uk-form-danger={delay !== Math.floor(delay)}
-                       uk-tooltip={delay !== Math.floor(delay) ? 'Delay must be an integer' : undefined}>
+				       class:uk-form-danger={delay !== Math.floor(delay)}
+				       uk-tooltip={delay !== Math.floor(delay) ? 'Delay must be an integer' : undefined}>
 			</div>
 			<div class="uk-width-auto">
 				{#if calculationStarted}
 					<label htmlFor="alignment-hack">&nbsp;<br></label>
-				<span on:click={stopCalc}
-				      uk-tooltip="Pause calculation"
-				      class="uk-icon-button pointer-cursor uk-animation-fade uk-animation-fast"
-				      uk-icon="stop-circle"></span>
+					<span on:click={stopCalc}
+					      uk-tooltip="Pause calculation"
+					      class="uk-icon-button pointer-cursor uk-animation-fade uk-animation-fast"
+					      uk-icon="stop-circle"></span>
 				{/if}
 			</div>
 			<div class="uk-width-auto">
@@ -112,7 +135,7 @@
 					      class="uk-icon-button uk-animation-fade uk-animation-fast"
 					      uk-icon="play-circle"
 					      disabled={delay !== Math.floor(delay)}
-                          class:pointer-cursor={delay === Math.floor(delay)}></span>
+					      class:pointer-cursor={delay === Math.floor(delay)}></span>
 				{:else if status === Status.RUNNING}
 					<span on:click={pauseCalc}
 					      uk-tooltip="Pause calculation"
@@ -122,7 +145,24 @@
 			</div>
 		</div>
 		<p>
-			{pi}
+			Value of pi: {pi}
 		</p>
+
+		<ul uk-accordion>
+			<li>
+				<a class="uk-accordion-title normal-text" href="#">Points</a>
+				<div class="uk-accordion-content">
+					{#if points.length !== 0}
+						<ul>
+							{#each points as point}
+								<li>point</li>
+							{/each}
+						</ul>
+					{:else}
+						No points generated yet. Press the <q>Run calculation</q> button to get going.
+					{/if}
+				</div>
+			</li>
+		</ul>
 	</div>
 </div>
