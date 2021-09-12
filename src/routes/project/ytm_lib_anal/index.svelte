0d8a601b65cc82cace0fa44b777237bae22c1aeb
d20cay <d20cayt@gmail.com>
2021-09-12 18:20:15 +0200

    Adds ytm lib analysis page

---
+++ src/routes/project/ytm_lib_anal/index.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {currentPage, ProjectPages} from "../../../stores";
+
+    currentPage.set(ProjectPages.YTM_LIB);
+</script>
+
+<svelte:head>
+	<title>d20cay | YT Music Library Analysis Project</title>
+</svelte:head>
+
+<h1 class="uk-text-center">
+	YT Music Library Analysis
+</h1>
+
+<p class="uk-text-center">
+	A tool to analyze and clean up you YTM library.
+</p>
