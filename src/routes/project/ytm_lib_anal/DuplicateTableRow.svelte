6427a8d2069b59901635b322ad71bd4d0ccfad05
d20cay <d20cayt@gmail.com>
2021-10-01 07:55:19 +0200

    Adds second testing version of analysis
    Apparently isn't crashing anymore *knock on wood*

---
+++ src/routes/project/ytm_lib_anal/DuplicateTableRow.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let duplicate;
+
+    function getSongShareString(videoId) {
+        return `https://music.youtube.com/watch?v=${videoId}&feature=share`;
+    }
+
+    function getPlaylistShareString(playlistId) {
+        return `https://music.youtube.com/playlist?list=${playlistId}&feature=share`;
+    }
+</script>
+
+
+<tr>
+	<td>
+		<a href={getSongShareString(duplicate.videoId)}>{duplicate.title}</a>
+	</td>
+	<td>
+		<a href={getSongShareString(duplicate.duplicateVideoId)}>{duplicate.duplicateTitle}</a>
+	</td>
+	<td>{duplicate.artists.map(a => a.name)}</td>
+	<td>{duplicate.duplicateArtists.map(a => a.name)}</td>
+	<td>
+		{#if duplicate.playlist === undefined}
+			Library Songs
+		{:else}
+			<a href={getPlaylistShareString(duplicate.playlist.id)}>{duplicate.playlist.title}</a>
+		{/if}
+	</td>
+	<td>
+		{#if duplicate.duplicatePlaylist === undefined} Library
+			Songs
+		{:else}
+			<a href={getPlaylistShareString(duplicate.duplicatePlaylist.id)}>{duplicate.duplicatePlaylist.title}</a>
+		{/if}
+	</td>
+</tr>
--- src/routes/project/ytm_lib_anal/index.svelte
+++ src/routes/project/ytm_lib_anal/index.svelte
@@ -1,53 +1,74 @@
 <script>
-	import {currentPage, ProjectPages} from "../../../stores";
-	import {
-		isProdInstance,
-		LoadingStatus,
-		NotificationPosition as Pos,
-		NotificationStatus as Status,
-		NotificationTimeout as Timeout,
-		notify
-	} from "../../../global";
+    import {currentPage, ProjectPages} from "../../../stores";
+    import {
+        isProdInstance,
+        LoadingStatus,
+        NotificationPosition as Pos,
+        NotificationStatus as Status,
+        NotificationTimeout as Timeout,
+        notify
+    } from "../../../global";
+    import {test} from "./test";
+    import {duplicateAnalysis} from "./analysis";
+    import DuplicatesTable from "./DuplicatesTable.svelte";
 
-	currentPage.set(ProjectPages.YTM_LIB);
+    currentPage.set(ProjectPages.YTM_LIB);
 
-	const INCORRECT_PAYLOAD = {code: -5, message: 'POST body in incorrect format.'};
+    const INCORRECT_PAYLOAD = {code: -5, message: 'POST body in incorrect format.'};
 
-	let loadingStatus = LoadingStatus.IDLE;
+    let loadingStatus = LoadingStatus.IDLE;
 
-	let cookie;
-	let x_goog_user;
-	let library;
+    let cookie;
+    let x_goog_user;
+    let library;
+    let analysis;
 
-	function inputsValid() {
-		return cookie !== "" && x_goog_user !== "";
-	}
+    $: analysis = analyze(library);
 
-	async function requestLibData() {
-		const prodApiUrl = `https://api.d20cay.com/ytm/lib`;
-		const devApiUrl = `http://localhost:8000/ytm/lib`;
+    function inputsValid() {
+        return cookie !== "" && x_goog_user !== "";
+    }
 
-		loadingStatus = LoadingStatus.LOADING;
-		await fetch(isProdInstance() ? prodApiUrl : devApiUrl, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({cookie, x_goog_user})
-		}).then(response => {
-			if (response.status === 422) {
-				throw INCORRECT_PAYLOAD;
-			}
-			return response.json();
-		}).then(data => {
-			loadingStatus = LoadingStatus.IDLE;
-			library = data;
-			notify('Successfully fetched library contents.',
-					Status.SUCCESS,
-					Pos.BOTTOM_LEFT,
-					Timeout.QUICK);
-		}).catch(() => notify('Error fetching library contents.', Status.DANGER, Pos.BOTTOM_LEFT, Timeout.CRITICAL));
-	}
+    async function requestLibData() {
+        const prodApiUrl = `https://api.d20cay.com/ytm/lib`;
+        const devApiUrl = `http://localhost:8000/ytm/lib`;
+
+        // TODO: remove for prod
+        library = test;
+        return;
+
+        loadingStatus = LoadingStatus.LOADING;
+        await fetch(isProdInstance() ? prodApiUrl : devApiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({cookie, x_goog_user})
+        }).then(response => {
+            if (response.status === 422) {
+                throw INCORRECT_PAYLOAD;
+            }
+            return response.json();
+        }).then(data => {
+            loadingStatus = LoadingStatus.IDLE;
+            library = data;
+            notify('Successfully fetched library contents.',
+                Status.SUCCESS,
+                Pos.BOTTOM_LEFT,
+                Timeout.QUICK);
+        }).catch(() => {
+            loadingStatus = LoadingStatus.IDLE;
+            notify('Error fetching library contents.', Status.DANGER, Pos.BOTTOM_LEFT, Timeout.CRITICAL);
+        });
+    }
+
+    function analyze(library) {
+        if (library === undefined) {
+            return undefined;
+        }
+        // TODO: Remove for prod
+        return {'duplicates': duplicateAnalysis(library)};
+    }
 </script>
 
 <svelte:head>
@@ -55,12 +76,12 @@
 </svelte:head>
 
 <h1 class="uk-text-center">
-	YT Music Library Analysis
+	classNameusic Library Analysis
 </h1>
 
-<div class="uk-width-2-3@m uk-align-center">
+<div class="uk-width-2-3@m ukclassNamen-center">
 	<p class="uk-text-center">
-		A tool to analyze and clean up your YTM library.
+		classNameool to analyze and clean up your YTM library.
 	</p>
 
 	<p>
@@ -107,5 +128,133 @@
 			</button>
 		</div>
 	</div>
+
+	{#if library !== undefined}
+
+		<ul uk-accordion>
+			<li>
+				<a class="uk-accordion-title" href="#">Duplicate analysis</a>
+				<!--Adds padding only on the left side to create an indent for every category.-->
+				<div class="uk-accordion-content uk-padding uk-padding-remove-top uk-padding-remove-right uk-padding-remove-bottom">
+					<p>
+						Possible duplicates are categorized in four ways:
+					</p>
+					<ol>
+						<li>ID Duplicates: The exact same song is duplicated.</li>
+						<li>Artist & Title Duplicates: The title and artist of the two songs are exactly the same, but
+							the ID
+							isn't.
+						</li>
+						<li>Title Duplicates: The title of the two songs are exactly the same, but the artist and ID are
+							different. These may simply be covers.
+						</li>
+						<li>Simplified Title Duplicates: The title, after removing anything in parentheses is exactly
+							the same, but the ID is different. The artist may be the same. These could be two different
+							versions from the same artist, e.g. it was created in a collaboration with another artist or
+							it's the acoustic version.
+						</li>
+					</ol>
+
+					<ul uk-accordion>
+						<li>
+							<a class="uk-accordion-title" href="#">Library Song Duplicates
+								({analysis.duplicates.library.totalDuplicates})</a>
+							<div class="uk-accordion-content uk-padding uk-padding-remove-top uk-padding-remove-right uk-padding-remove-bottom">
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">ID Duplicates
+											({analysis.duplicates.library.idDuplicates.length})</a>
+										<div class="uk-accordion-content uk-padding uk-padding-remove-top uk-padding-remove-right uk-padding-remove-bottom">
+											<DuplicatesTable duplicates={analysis.duplicates.library.idDuplicates}/>
+										</div>
+									</li>
+								</ul>
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">Title & Artist Duplicates
+											({analysis.duplicates.library.titleArtistDuplicates.length})</a>
+										<div class="uk-accordion-content">
+											<DuplicatesTable
+													duplicates={analysis.duplicates.library.titleArtistDuplicates}/>
+										</div>
+									</li>
+								</ul>
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">Title Duplicates
+											({analysis.duplicates.library.titleDuplicates.length})</a>
+										<div class="uk-accordion-content">
+											<DuplicatesTable duplicates={analysis.duplicates.library.titleDuplicates}/>
+										</div>
+									</li>
+								</ul>
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">Simplified Title Duplicates
+											({analysis.duplicates.library.simpleTitleDuplicates.length})</a>
+										<div class="uk-accordion-content">
+											<DuplicatesTable
+													duplicates={analysis.duplicates.library.simpleTitleDuplicates}/>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li>
+							<a class="uk-accordion-title" href="#">Playlist Duplicates
+								({analysis.duplicates.playlists.totalDuplicates})</a>
+							<div class="uk-accordion-content uk-padding uk-padding-remove-top uk-padding-remove-right uk-padding-remove-bottom">
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">ID Duplicates
+											({analysis.duplicates.playlists.idDuplicates.length})</a>
+										<div class="uk-accordion-content uk-padding uk-padding-remove-top uk-padding-remove-right uk-padding-remove-bottom">
+											<DuplicatesTable
+													duplicates={analysis.duplicates.playlists.idDuplicates}/>
+										</div>
+									</li>
+								</ul>
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">Title & Artist Duplicates
+											({analysis.duplicates.library.titleArtistDuplicates.length})</a>
+										<div class="uk-accordion-content">
+											<DuplicatesTable
+													duplicates={analysis.duplicates.playlists.titleArtistDuplicates}/>
+										</div>
+									</li>
+								</ul>
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">Title Duplicates
+											({analysis.duplicates.playlists.titleDuplicates.length})</a>
+										<div class="uk-accordion-content">
+											<DuplicatesTable
+													duplicates={analysis.duplicates.playlists.titleDuplicates}/>
+										</div>
+									</li>
+								</ul>
+								<ul uk-accordion>
+									<li>
+										<a class="uk-accordion-title" href="#">Simplified Title Duplicates
+											({analysis.duplicates.playlists.simpleTitleDuplicates.length})</a>
+										<div class="uk-accordion-content">
+											<DuplicatesTable
+													duplicates={analysis.duplicates.playlists.simpleTitleDuplicates}/>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</li>
+		</ul>
+	{:else}
+		<p class="uk-text-center uk-margin-medium">
+			Nothing here yet. Enter your credentials to show information and options here.
+		</p>
+	{/if}
+
 </div>
 
