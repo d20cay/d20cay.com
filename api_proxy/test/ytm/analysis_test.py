6b870d88e6a08d620007d7dff11ef59ee56f18ba
Lani Wagner <lani.wagner@students.fhnw.ch>
2021-11-24 07:23:49 +0100

    Splits analysis tests into separate functions

--- api_proxy/test/ytm/analysis_test.py
+++ api_proxy/test/ytm/analysis_test.py
@@ -11,70 +11,94 @@ class TestAnalysis(unittest.TestCase):
         self.assertEqual(simplify_title("Halle(lujah(ha))"), "Halle", "Should remove (lujah(ha)) and leave Halle")
         self.assertEqual(simplify_title("Halle(lujah(ha)"), "Halle", "Should remove (lujah(ha) and leave Halle")
 
-    DUPLICATE_ID_LIBRARY_LIBRARY_SONG_LIBRARY = {
-        "songs": [{
-            "videoId": "a",
-            "title": "Heat Waves",
-            "artists": [
-                {"name": "Glass animals", "id": "b"},
-                {"name": "iann dior", "id": "c"}
-            ]}, {
-            "videoId": "a",
-            "title": "Heat Waves",
-            "artists": [
-                {"name": "Glass animals", "id": "b"},
-                {"name": "iann dior", "id": "c"}
-            ]
-        }],
-        "playlists": []
-    }
-    DUPLICATE_ID_LIBRARY_LIBRARY_SONG_ISSUES = {"library": {"totalDuplicateCount": 1, "idDuplicates": [{
-        "type": DuplicateIssueType.ID_DUPLICATE,
-        "videoId": "a",
-        "title": "Heat Waves",
-        "artists": [
-            {"name": "Glass animals", "id": "b"},
-            {"name": "iann dior", "id": "c"}
-        ],
-        "playlist": None,
-        "duplicateVideoId": "a",
-        "duplicateTitle": "Heat Waves",
-        "duplicateArtists": [
-            {"name": "Glass animals", "id": "b"},
-            {"name": "iann dior", "id": "c"}
-        ],
-        "duplicatePlaylist": None,
-    }], "simpleTitleDuplicates": [], "titleArtistDuplicates": [], "titleDuplicates": []},
-                                                "playlists": {"totalDuplicateCount": 0, "idDuplicates": [],
-                                                              "simpleTitleDuplicates": [], "titleArtistDuplicates": [],
-                                                              "titleDuplicates": []}}
-
-    DUPLICATE_ID_LIBRARY_OTHER_SONG_LIBRARY = {
-        "songs": [{
-            "videoId": "a",
-            "title": "Heat Waves",
-            "artists": [
-                {"name": "Glass animals", "id": "b"},
-                {"name": "iann dior", "id": "c"}
-            ]
-        }],
-        "playlists": [{
-            "id": "d",
-            "title": "Best",
-            "tracks": [{
+    def test_ytm_analyze_id_duplicate_library_library(self):
+        DUPLICATE_ID_LIBRARY_LIBRARY_SONG_LIBRARY = {
+            "songs": [{
+                "videoId": "a",
+                "title": "Heat Waves",
+                "artists": [
+                    {"name": "Glass animals", "id": "b"},
+                    {"name": "iann dior", "id": "c"}
+                ]}, {
                 "videoId": "a",
                 "title": "Heat Waves",
                 "artists": [
                     {"name": "Glass animals", "id": "b"},
                     {"name": "iann dior", "id": "c"}
                 ]
-            }]
-        }]
-    }
+            }],
+            "playlists": []
+        }
+        DUPLICATE_ID_LIBRARY_LIBRARY_SONG_ISSUES = {"library": {"totalDuplicateCount": 1, "idDuplicates": [{
+            "type": DuplicateIssueType.ID_DUPLICATE,
+            "videoId": "a",
+            "title": "Heat Waves",
+            "artists": [
+                {"name": "Glass animals", "id": "b"},
+                {"name": "iann dior", "id": "c"}
+            ],
+            "playlist": None,
+            "duplicateVideoId": "a",
+            "duplicateTitle": "Heat Waves",
+            "duplicateArtists": [
+                {"name": "Glass animals", "id": "b"},
+                {"name": "iann dior", "id": "c"}
+            ],
+            "duplicatePlaylist": None,
+        }], "simpleTitleDuplicates": [], "titleArtistDuplicates": [], "titleDuplicates": []},
+                                                    "playlists": {"totalDuplicateCount": 0, "idDuplicates": [],
+                                                                  "simpleTitleDuplicates": [],
+                                                                  "titleArtistDuplicates": [],
+                                                                  "titleDuplicates": []}}
+        self.assertEqual(ytm_analyze(DUPLICATE_ID_LIBRARY_LIBRARY_SONG_LIBRARY),
+                         DUPLICATE_ID_LIBRARY_LIBRARY_SONG_ISSUES)
 
-    def test_ytm_analyze(self):
-        self.assertEqual(ytm_analyze(self.DUPLICATE_ID_LIBRARY_LIBRARY_SONG_LIBRARY),
-                         self.DUPLICATE_ID_LIBRARY_LIBRARY_SONG_ISSUES)
+    def test_ytm_analyze_id_duplicate_library_playlists(self):
+        DUPLICATE_ID_LIBRARY_PLAYLISTS_SONG_LIBRARY = {
+            "songs": [{
+                "videoId": "a",
+                "title": "Heat Waves",
+                "artists": [
+                    {"name": "Glass animals", "id": "b"},
+                    {"name": "iann dior", "id": "c"}
+                ]
+            }],
+            "playlists": [{
+                "id": "d",
+                "title": "Best",
+                "tracks": [{
+                    "videoId": "a",
+                    "title": "Heat Waves",
+                    "artists": [
+                        {"name": "Glass animals", "id": "b"},
+                        {"name": "iann dior", "id": "c"}
+                    ]
+                }]
+            }]
+        }
+        DUPLICATE_ID_LIBRARY_PLAYLISTS_SONG_ISSUES = {"library": {"totalDuplicateCount": 1, "idDuplicates": [{
+            "type": DuplicateIssueType.ID_DUPLICATE,
+            "videoId": "a",
+            "title": "Heat Waves",
+            "artists": [
+                {"name": "Glass animals", "id": "b"},
+                {"name": "iann dior", "id": "c"}
+            ],
+            "playlist": None,
+            "duplicateVideoId": "a",
+            "duplicateTitle": "Heat Waves",
+            "duplicateArtists": [
+                {"name": "Glass animals", "id": "b"},
+                {"name": "iann dior", "id": "c"}
+            ],
+            "duplicatePlaylist": {"id": "d", "title": "Best"},
+        }], "simpleTitleDuplicates": [], "titleArtistDuplicates": [], "titleDuplicates": []},
+                                                      "playlists": {"totalDuplicateCount": 0, "idDuplicates": [],
+                                                                    "simpleTitleDuplicates": [],
+                                                                    "titleArtistDuplicates": [],
+                                                                    "titleDuplicates": []}}
+        self.assertEqual(ytm_analyze(DUPLICATE_ID_LIBRARY_PLAYLISTS_SONG_LIBRARY),
+                         DUPLICATE_ID_LIBRARY_PLAYLISTS_SONG_ISSUES)
 
 
 if __name__ == "__main__":
