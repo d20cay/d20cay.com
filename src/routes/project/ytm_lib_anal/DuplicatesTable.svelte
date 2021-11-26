a626acb6a3ccea859e72ec1c30a9f20f70340d40
Lani Wagner <lani.wagner@students.fhnw.ch>
2021-11-26 09:21:01 +0100

    Reduces the amount of elements shown for issues in an effort to improve display performance

--- src/routes/project/ytm_lib_anal/DuplicatesTable.svelte
+++ src/routes/project/ytm_lib_anal/DuplicatesTable.svelte
@@ -1,23 +1,35 @@
 <script>
-    import DuplicateTableRow from "./DuplicateTableRow.svelte";
+	import DuplicateTableRow from "./DuplicateTableRow.svelte";
 
-    export let duplicates;
+	export let duplicates;
+
+	const MAX_DEFAULT_ISSUE_COUNT = 30;
+	const ISSUE_ITEM_INCREMENT = 50;
+
+	let issueItemCount = MAX_DEFAULT_ISSUE_COUNT;
 </script>
 
 <table class="uk-table uk-table-overflow-auto">
 	<thead>
-	<tr>
-		<th>Song Title</th>
-		<th>Duplicate Song Title</th>
-		<th>Artists</th>
-		<th>Duplicate Artists</th>
-		<th>Playlist</th>
-		<th>Duplicate Playlist</th>
-	</tr>
+		<tr>
+			<th>Song Title</th>
+			<th>Duplicate Song Title</th>
+			<th>Artists</th>
+			<th>Duplicate Artists</th>
+			<th>Playlist</th>
+			<th>Duplicate Playlist</th>
+		</tr>
 	</thead>
 	<tbody>
-	{#each duplicates as duplicate}
-		<DuplicateTableRow {duplicate}/>
-	{/each}
+		{#each duplicates as duplicate, i}
+			{#if i < issueItemCount}
+				<DuplicateTableRow {duplicate}/>
+			{/if}
+			{#if i === issueItemCount - 1 && issueItemCount < duplicates.length}
+				<li class="more-li"><a on:click={() => issueItemCount += ISSUE_ITEM_INCREMENT}>+{duplicates.length -
+				issueItemCount} more</a>
+				</li>
+			{/if}
+		{/each}
 	</tbody>
 </table>
