f5e600aae36497c0e5c38850ce54d9357dcbbe32
Lani Wagner <lani.wagner@students.fhnw.ch>
2021-12-03 19:02:37 +0100

    Strips library before analysis to process and return less data in hopes of reducing run time

--- api_proxy/ytm/ytm.py
+++ api_proxy/ytm/ytm.py
@@ -8,4 +8,16 @@ def get_library(cookie, x_goog_authuser):
     playlists = [api.get_playlist(lpl['playlistId'], limit=10 ** 9) for lpl in api.get_library_playlists()]
     songs = api.get_library_songs(limit=10 ** 9)
 
-    return {'songs': songs, 'playlists': playlists}
+    return strip_library(songs, playlists)
+
+
+def strip_library(songs, playlists):
+    playlists = list(map(
+        lambda playlist: {"id": playlist["id"], "title": playlist["title"], "tracks": strip_songs(playlist["tracks"])},
+        playlists))
+    return {"songs": strip_songs(songs), "playlists": playlists}
+
+
+def strip_songs(songs):
+    return list(
+        map(lambda song: {"videoId": song["videoId"], "title": song["title"], "artists": song["artists"]}, songs))
